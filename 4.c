761d24cbffd242e2c15f70d2c466f0dc274b16ae
mahbubajothi-bit <mahbuba.jothi@auw.edu.bd>
2026-02-06 19:29:50 +0600

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    int x = 5;
+    printf("Initial value: %d\n", x);
+
+    x++; // increment
+    printf("After increment: %d\n", x);
+
+    x--; // decrement
+    printf("After decrement: %d\n", x);
+
+    return 0;
+}
